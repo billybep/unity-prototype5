b1a896630df9fefd579040e2bf60f052fc8dda01
billybep <billy.pesoth@gmail.com>
2022-11-13 00:14:07 +0800

    add 3 level dificulty

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -5,17 +5,30 @@ using UnityEngine.UI;
 
 public class DifficultyButton : MonoBehaviour
 {
-    public Button button;
+    private GameManager gameManager;
+
+    private Button button;
+
+    public int dificulty;
 
     // Start is called before the first frame update
     void Start()
     {
         button = GetComponent<Button>();
+        button.onClick.AddListener(SetDificulty);
+
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SetDificulty()
+    {
+        Debug.Log(gameObject.name + " was clicked!");
+        gameManager.StartGame(dificulty);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,7 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
+    public GameObject titleScreen;
 
     private float spawnRate = 1.0f;
     private int score;
@@ -20,11 +21,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -59,5 +56,16 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int dificulty)
+    {
+        isGameActive = true;
+        titleScreen.gameObject.SetActive(false);
+
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        spawnRate /= dificulty;
     }
 }
