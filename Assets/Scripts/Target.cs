0256b63554fba6deb0df7832ffab28900838fb8b
billybep <billy.pesoth@gmail.com>
2022-11-12 11:01:10 +0800

    add canvas to store score

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
 
     private float minSpeed = 12;
     private float maxSpeed = 16;
@@ -16,6 +17,7 @@ public class Target : MonoBehaviour
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
         // Force target to up on random between Random Range
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
@@ -52,6 +54,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.UpdateScore(5);
     }
 
     private void OnTriggerEnter(Collider other)
